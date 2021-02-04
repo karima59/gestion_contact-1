14298f5cf8b575dca267591dc5bd71728bc077e9
Samuel Lassoie <samuel.lassoie+1@gmail.com>
2021-02-04 18:28:53 +0100

    wip

--- contact.php
+++ contact.php
@@ -12,42 +12,37 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-6">
-                <form action="save_contact.php" method="post">
-                    <div class="form-group row">
-                        <label for="nom" class="col-sm-4 col-form-label">Nom</label>
-                        <div class="col-sm-8">
-                            <input type="text" class="form-control" name="nom" id="nom">
-                        </div>
+            <div class="col-md-6 offset-md-3">
+                <form action="save_contact.php" method="post" novalidate>
+                    <div class="form-group">
+                        <label for="nom">Nom</label>
+                        <input type="text" name="nom" class="form-control" id="nom">
                     </div>
-                    <div class="form-group row">
-                        <label for="prenom" class="col-sm-4 col-form-label">Prénom</label>
-                        <div class="col-sm-8">
-                            <input type="text" class="form-control" name="prenom" id="prenom">
-                        </div>
+                    <div class="form-group">
+                        <label for="prenom">Prénom</label>
+                        <input type="text" name="prenom" class="form-control" id="prenom">
                     </div>
-                    <div class="form-group row">
-                        <label for="date_de_naissance" class="col-sm-4 col-form-label">Date de naissance</label>
-                        <div class="col-sm-8">
-                            <input type="date" class="form-control" name="date_de_naissance" id="date_de_naissance">
-                        </div>
+                    <div class="form-group">
+                        <label for="date_de_naissance">Date de naissance</label>
+                        <input type="date" name="date_de_naissance" class="form-control" id="date_de_naissance">
                     </div>
-                    <div class="form-group row">
-                        <label for="civilite" class="col-sm-4 col-form-label">Civilite</label>
-                        <div class="col-sm-8">
-                            <input type="text" class="form-control" name="civilite" id="civilite">
-                        </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="civilite" id="civilite1" value="1">
+                        <label class="form-check-label" for="civilite1">
+                            Monsieur
+                        </label>
                     </div>
-                    <div class="form-group row">
-                        <button type="submit" class="btn btn-primary">Envoyer</button>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="civilite" id="civilite2" value="2">
+                        <label class="form-check-label" for="civilite2">
+                            Madame
+                        </label>
                     </div>
+                    <button type="submit" class="btn btn-primary mt-4">Enregistrer</button>
                 </form>
             </div>
         </div>
-    </div>
-
-
-
 
 
 </body>
